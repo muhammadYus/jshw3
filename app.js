ab71345c8e5c7c7dd1c3af76213ec253ea5509d1
muhammadYus <74594824+muhammadYus@users.noreply.github.com>
2021-12-23 17:05:54 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+let student = {
+    Name:'Boyir',
+    id: 46734674,
+    age: 15,
+};
+const {Name,id, age} = student;
+console.log(Name,id,age);
+2 === true
+false
+0 === false
+false
+1000 == 1000
+true
+let
+a = null;
+bag = null;
+if (age == bag);
+    console.log("true");
+console.log(age < bag);
+
+    
